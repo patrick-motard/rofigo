98e699eed342ff3c60264e0dff2af4e038d4912e
patrick-motard <motard19@gmail.com>
2019-07-14 00:38:32 -0500

    is now a package instead of program

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/patrick-motard/rofigo
 
 go 1.12
-
-require rsc.io/quote v1.5.2
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
-package main
+package rofigo
 
 import (
 	"fmt"
-	"os/exec"
 	"log"
+	"os/exec"
 	// "os"
 	"strings"
 )
@@ -13,12 +13,12 @@ func Hello() string {
 }
 
 type Page struct {
-	Options []string
+	Options   []string
 	Selection string
 }
 
 func New(options ...string) *Page {
-	return &Page {
+	return &Page{
 		Options: options,
 	}
 }
@@ -34,33 +34,33 @@ func (p *Page) Show() {
 	fmt.Println(p.Selection)
 }
 
-func main() {
-	page1 := New("option1", "option2", "option2")
-	page1.Show()
-	log.Println(page1.Selection)
-	// var options = "1\n2\n3\n"
+// func main() {
+// 	page1 := New("option1", "option2", "option2")
+// 	page1.Show()
+// 	log.Println(page1.Selection)
+// 	// var options = "1\n2\n3\n"
 
-	// options := []string{"one", "two", "three", "four"}
-	// command := fmt.Sprintf("echo -e '%s' | rofi -dmenu", strings.Join(options, "\n"))
-	// log.Println(command)
-	// // os.Exit(0)
-	// othercommand := "echo -e 'hello\nhi\nello' | rofi -dmenu"
-	// log.Print(othercommand)
-	// // cmd := exec.Command("/bin/sh", "-c", "'echo -e \"1\n2\n3\n\" | rofi -dmenu'")
+// 	// options := []string{"one", "two", "three", "four"}
+// 	// command := fmt.Sprintf("echo -e '%s' | rofi -dmenu", strings.Join(options, "\n"))
+// 	// log.Println(command)
+// 	// // os.Exit(0)
+// 	// othercommand := "echo -e 'hello\nhi\nello' | rofi -dmenu"
+// 	// log.Print(othercommand)
+// 	// // cmd := exec.Command("/bin/sh", "-c", "'echo -e \"1\n2\n3\n\" | rofi -dmenu'")
 
-	// cmd := exec.Command("/bin/sh", "-c", command)
+// 	// cmd := exec.Command("/bin/sh", "-c", command)
 
-	// // cmd := exec.Command("/bin/sh", "-c", command)
-	// // cmd := exec.Command("rofi", "-show", "drun")
+// 	// // cmd := exec.Command("/bin/sh", "-c", command)
+// 	// // cmd := exec.Command("rofi", "-show", "drun")
 
-	// // uncomment to pipe to stdout of go program
-	// // cmd.Stdout = os.Stdout
-	// // cmd.Stderr = os.Stderr
-	// out, err := cmd.Output()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// }
-	// log.Print(string(out))
+// 	// // uncomment to pipe to stdout of go program
+// 	// // cmd.Stdout = os.Stdout
+// 	// // cmd.Stderr = os.Stderr
+// 	// out, err := cmd.Output()
+// 	// if err != nil {
+// 	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
+// 	// }
+// 	// log.Print(string(out))
 
-	// fmt.Println(Hello())
-}
+// 	// fmt.Println(Hello())
+// }
